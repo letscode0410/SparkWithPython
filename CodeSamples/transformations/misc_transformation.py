4a77938c56b22580981e26fb883ef0d0af5f933c
Ravi Kumar P <ravikumar.p536@gmail.com>
2022-12-28 19:52:16 +0530

    drop and drop duplicates

--- CodeSamples/transformations/misc_transformation.py
+++ CodeSamples/transformations/misc_transformation.py
@@ -1,7 +1,7 @@
 from pyspark import SparkConf
 from pyspark.sql import SparkSession
-from pyspark.sql.functions import expr, monotonically_increasing_id
-from pyspark.sql.types import  IntegerType
+from pyspark.sql.functions import expr, monotonically_increasing_id, col, when
+from pyspark.sql.types import IntegerType
 
 if __name__ == '__main__':
     sparkConf = SparkConf()
@@ -18,7 +18,7 @@ if __name__ == '__main__':
                  ("Rosy", "7", "8", "63"),  # 1963
                  ("Abdul", "23", "5", "81")]  # 1981
 
-    raw_df = spark.createDataFrame(data_list).toDF("name", "age", "month", "year").repartition(3)
+    raw_df = spark.createDataFrame(data_list).toDF("name", "day", "month", "year").repartition(3)
     raw_df.printSchema()
     # final_df = raw_df.withColumn("id",monotonically_increasing_id()) \
     #     .withColumn("year", expr("""
@@ -36,11 +36,30 @@ if __name__ == '__main__':
     #                end
     #                """))
 
+    # final_df = raw_df.withColumn("id", monotonically_increasing_id()) \
+    #     .withColumn("year", expr("""
+    #                   case when year < 21 then year+ 2000
+    #                   when year < 100 then year + 1900
+    #                   else year
+    #                   end
+    #                   """).cast(IntegerType()))
+
     final_df = raw_df.withColumn("id", monotonically_increasing_id()) \
-        .withColumn("year", expr("""
-                      case when year < 21 then year+ 2000
-                      when year < 100 then year + 1900
-                      else year
-                      end
-                      """).cast(IntegerType()))
+        .withColumn("year", col("year").cast(IntegerType())) \
+        .withColumn("day", col("day").cast(IntegerType())) \
+        .withColumn("month", expr("cast(month as int)")) \
+        .withColumn("year", when(col("year") < 21, col("year") + 2000) \
+                    .when(col("year") < 100, col("year") + 1900) \
+                    .otherwise(col("year"))) \
+        .withColumn("dob", expr("to_date(concat(day,'/',month,'/',year),'d/M/y')"))\
+        .drop("year", "day", "month") \
+        .dropDuplicates(["name", "dob"]) \
+        .sort("dob")
+
+    # .withColumn("year", expr("""
+    #                   case when year < 21 then year + 2000
+    #                   when year < 100 then year + 1900
+    #                   else year
+    #                   end
+    #                   """))
     final_df.show()
